e2e8fec18129826e6dd26c684d8ffdc5d741c1a2
17931837B <tomoya.bb1014@gmail.com>
2025-04-22 11:17:53 +0900

    ex00 done

---
+++ ex00/whatever.hpp
@@ -0,0 +1,37 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+
+template <typename T>
+void	swap(T& a, T& b)
+{
+	T	tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T	min(T const& one, T const& sec)
+{
+	if (one == sec)
+		return (sec);
+	else if (one > sec)
+		return (sec);
+	else
+		return (one);
+}
+
+template <typename T>
+T	max(T const& one, T const& sec)
+{
+	if (one == sec)
+		return (sec);
+	else if (one > sec)
+		return (one);
+	else
+		return (sec);
+}
+
+#endif
